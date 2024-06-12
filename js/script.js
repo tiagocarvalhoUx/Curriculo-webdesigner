32e950ba5de1bb8335175244eed57a69d0ed13f7
Tiago Carvalho <136613404+tiago-bozo@users.noreply.github.com>
2024-06-12 13:22:18 -0300

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*================================== typing animation ===================*/
+var typed = new Typed(".typing", {
+    strings:["","Web Designer", "Web Developer", "Graphic Designer"],
+    typeSpeed:100,
+    BackSpedd:60,
+    loop:true
+})
